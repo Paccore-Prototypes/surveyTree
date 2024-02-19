ba940fc37bdc0da9bd92db3dda5d7089b73f9460
Alavalaiah123 <alavalaiah.b@paccore.com>
2024-02-19 17:17:52 +0530

    pushing all files into base branch.

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:custom_scale/src/callingFile.dart';
+import 'package:custom_scale/src/survey_tree.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ImportingProperties()
+    );
+  }
+}
+
