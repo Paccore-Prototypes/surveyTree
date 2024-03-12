288d9fe9dda1605c7614c279b39897ef41c92efc
Alavalaiah123 <alavalaiah.b@paccore.com>
2024-03-12 15:07:54 +0530

    conflicts cleared

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:infosurvey/infosurvey.dart';
 import 'package:infosurvey_example/callingFile.dart';
 
 void main() {
